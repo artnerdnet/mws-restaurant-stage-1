85669e0f188e86d019c8707bf46c8db24f252769
@dianavile <diana.vile@gmail.com>
2018-06-25 16:34:52 +0200

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -3,7 +3,7 @@
  * From: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
  */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../sw.js', { scope: '' })
+  navigator.serviceWorker.register('/sw.js', { scope: '' })
     .then(function (reg) {
       // registration worked
       console.log('Registration succeeded. Scope is ' + reg.scope);
