a372a5bb938e41c277cf70d77414e63eeb84b8cc
@dianavile <diana.vile@gmail.com>
2018-06-25 16:10:35 +0200

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,16 +1,28 @@
 /**
- * Add service worker registration file.
+ * Register ServiceWorker.
+ * From: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
  */
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+  navigator.serviceWorker.register('../sw.js', { scope: '' })
+    .then(function (reg) {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function (error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
     });
-  });
+}
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register("index.js")
+    .then(function(reg) {
+      console.log("Service Worker installed successfully!");
+     
+    })
+    .catch(function(err) {
+      console.log("Service Worker failed to install!");
+    });
 }
 
 /**
