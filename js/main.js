b949c5ac6b1ba28a3eae3b6493823589a955aa01
@dianavile <diana.vile@gmail.com>
2018-06-26 10:01:01 +0200

    Fix: missing alt tag
    Add  "image.alt" to DBHelper.js file

--- js/main.js
+++ js/main.js
@@ -170,6 +170,7 @@ createRestaurantHTML = restaurant => {
 
   const image = document.createElement("img");
   image.className = "restaurant-img";
+  image.alt = DBHelper.imageAltForRestaurant(restaurant);  
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
